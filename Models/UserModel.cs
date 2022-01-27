207024f71a82b0abfc0137f1a06aaff7f7ee8f06
JoshBlood <finchironacc@gmail.com>
2022-01-27 17:54:35 +0000

    Add UserModel class

---
+++ Models/UserModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace F1TeamApplication.Models
+{
+    public class UserModel
+    {
+        public int UserId { get; set; }
+        public string Username { get; set; }    
+        public string GivenName { get; set; }   
+        public string Surname { get; set; }
+        public string Email { get; set; }   
+        public string Role { get; set; }
+        public int MobileNumber { get; set; }
+    }
+}
