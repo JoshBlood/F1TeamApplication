c02f56e055a1c170001cee28600eb2d7098bf487
JoshBlood <finchironacc@gmail.com>
2022-01-27 18:35:01 +0000

    Add User

--- Models/UserConstants.cs
+++ Models/UserConstants.cs
@@ -7,6 +7,10 @@ namespace F1TeamApplication.Models
 {
     public class UserConstants
     {
-
+        public static List<User> Users = new List<User>()
+        {
+            new User() { UserId = 104, Username = "Josh", GivenName = "Josh", Surname = "Bloodworth", Email = "Josh@gmail.com",
+                Role = "Admin", MobileNumber = 01234 },
+        };
     }
 }
